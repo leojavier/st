06d8df9689289d4fdd92819f4fa6b150ddcaa5d7
leojavier <info@leojavier.com>
2016-01-08 18:01:16 -0500

    adding files

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+	// Let's scrape Anchorman 2
+	url = 'http://www.express.com/clothing/men/ties/cat/cat280005';
+
+	request(url, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+
+			var title, release, rating;
+			var json = { image : "", release : "", rating : ""};
+
+			json.image=$('.product-image').first().attr('src');
+
+	        $('.star-box-giga-star').filter(function(){
+	        	var data = $(this);
+	        	rating = data.text();
+
+	        	json.rating = rating;
+	        })
+		}
+
+		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+        	console.log('File successfully written! - Check your project directory for the output.json file');
+        })
+
+        res.send('Check your console!')
+	})
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
+exports = module.exports = app; 	
